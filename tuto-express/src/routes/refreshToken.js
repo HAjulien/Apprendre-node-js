5caf3c6f570560592213aac636482b00e70b866d
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-12-01 23:02:52 +0100

    vérification pour user refreshToken

--- tuto-express/src/routes/refreshToken.js
+++ tuto-express/src/routes/refreshToken.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken"
 import * as dotenv from 'dotenv'
 import { generateToken } from "../../helpers.js"
+import { User } from "../db/sequelize.js"
 dotenv.config()
 
 
@@ -13,17 +14,26 @@ export const refreshToken = (app) => {
             return res.sendStatus(404)
         }
 
-        jwt.verify(token, process.env.PRIVATE_REFRESH_KEY, (error, user) => {
+        jwt.verify(token, process.env.PRIVATE_REFRESH_KEY, async (error, user) => {
             if (error){
                 return res.sendStatus(401)
             }
 
-            delete user.iat
-            delete user.exp
-            const refreshedToken = generateToken(user);
-            res.send({
-                accessToken : refreshedToken
-            })
+            try {
+                const searchUser = await User.findByPk(user.userId)
+                if(searchUser === null ){
+                    throw new Error("Le user n'existe plus.")
+                }
+                delete user.iat
+                delete user.exp
+                const refreshedToken = generateToken(user);
+                res.send({
+                    accessToken : refreshedToken
+                })
+            } catch (e) {
+                const message = "Le user n'existe plus."
+                return res.json({message})                
+            }
         })
 
     })
